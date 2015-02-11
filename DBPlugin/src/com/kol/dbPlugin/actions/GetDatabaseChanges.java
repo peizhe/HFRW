9d6df5379502d0064571bf2637855cb3f55c25a6
Olexandr <kolexandr.13@gmail.com>
2015-02-11 02:14:37 +0200

    db

--- DBPlugin/src/com/kol/dbPlugin/actions/GetDatabaseChanges.java
+++ DBPlugin/src/com/kol/dbPlugin/actions/GetDatabaseChanges.java
@@ -1,10 +1,14 @@
 package com.kol.dbPlugin.actions;
 
+import com.google.common.base.Splitter;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.kol.dbPlugin.C;
 import org.jetbrains.annotations.NotNull;
+
+import java.nio.file.Paths;
 
 public class GetDatabaseChanges extends BaseAction {
 
@@ -13,7 +17,35 @@ public class GetDatabaseChanges extends BaseAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
         super.actionPerformed(event);
+        final Project project = event.getProject();
         final VirtualFile actionFile = event.getData(LangDataKeys.VIRTUAL_FILE);
+        if(null == actionFile || null == project || null == actionFile.getCanonicalPath()) {
+            return;
+        }
+        final String canonicalPath = Paths.get(actionFile.getCanonicalPath()).toString();
+        final String basePath = Paths.get(project.getBasePath()).toString();
+        final String replace = canonicalPath.replace(basePath, "").replaceAll("^\\\\", "");
+        final String dataFolders;
+        if(replace.startsWith(C.PLUGIN_DIRECTORY_NAME)) {
+            dataFolders = replace.replaceAll("^" + C.PLUGIN_DIRECTORY_NAME, "");
+        } else {
+            throw new RuntimeException("");
+        }
+
+        final Iterable<String> split = Splitter.on("\\").omitEmptyStrings().limit(2).split(dataFolders);
+        System.out.println(split);
+
+//        event.getRequiredData()
+
+        /*final ConnectionData data = new ConnectionData(
+                "jdbc:mysql://" + url,
+                credentials.getUsername(),
+                credentials.getPassword(),
+                "com.mysql.jdbc.Driver"
+        );
+        final DatabaseOperations jdbc = new DatabaseTemplate(data);
+        final Integer num = jdbc.queryForObject("select count(*) from information_schema.TRIGGERS", Integer.class);
+*/
         int i = 0;
         /*Credentials credentials = safeGetCredentials("general");
         if(null == credentials) {
@@ -22,8 +54,6 @@ public class GetDatabaseChanges extends BaseAction {
             credentials = new Credentials(auth.getUsername(), auth.getPassword());
             safeSaveCredentials(credentials);
         }
-        final String url = Messages.showInputDialog(project, "Input URL to your database", "Database URL", Messages.getQuestionIcon());
-
         final ConnectionData data = new ConnectionData("jdbc:mysql://" + url, credentials.getUsername(), credentials.getPassword(), "com.mysql.jdbc.Driver");
         final boolean isOk = DatabaseConnector.isCorrectDBProperties(data);
         final DatabaseOperations jdbc = new DatabaseTemplate(data);
